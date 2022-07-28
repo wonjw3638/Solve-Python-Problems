ab65978aea9f0824ae9e1e68ff3770e7fe570de2
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-07-28 17:04:57 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,15 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for i in range(n)]
+    for com in range(n):
+        if visited[com] == False:
+            DFS(n, computers, com, visited)
+            answer += 1
+    return answer
+
+def DFS(n, computers, com, visited):
+    visited[com] = True
+    for connect in range(n):
+        if connect != com and computers[com][connect] == 1:
+            if visited[connect] == False:
+                DFS(n, computers, connect, visited)
