1b7632e88b2aa81a1d160c82af347bc22e39c4ef
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-08-17 02:45:47 +0900

    [Silver V] Title: 칠무해, Time: 3248 ms, Memory: 269736 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.py
+++ 백준/Silver/14729. 칠무해/칠무해.py
@@ -8,6 +8,25 @@ num_list = list()
 for _ in range(N):
     num_list.append(float(input()))
 
-num_list.sort()
+min_seven = num_list[:7]
+
 for i in range(7):
-    print('{:.3f}'.format(num_list[i]))
+    minIdx = i
+    for j in range(i+1,7):
+        if min_seven[minIdx] > min_seven[j]:
+            minIdx = j
+    min_seven[minIdx], min_seven[i] = min_seven[i], min_seven[minIdx]
+
+for num in num_list[7:]:
+    if num >= min_seven[6]:
+        continue
+    else:
+        min_seven[6] = 101
+        for i in range(7):
+            if num < min_seven[i]:
+                min_seven.insert(i, num)
+                break
+            else: continue
+
+for i in range(7):
+    print('{:.3f}'.format(min_seven[i]))
