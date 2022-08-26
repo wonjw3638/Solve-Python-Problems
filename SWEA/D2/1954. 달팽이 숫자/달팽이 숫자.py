bcce5f7b748968dc7220eb415b3c3be91b69ec49
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-08-26 16:29:43 +0900

    [D2] Title: 달팽이 숫자, Time: 155 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,26 +1,38 @@
 T = int(input())
 
-# 우하좌상 순으로 
-di = [0, +1, 0, -1]
-dj = [+1, 0, -1, 0]
+def snail(N):
+    i = j = d = 0
+    num = 1
+    arr[i][j] = num
+
+    if N == 1:
+        return 
+
+    while num < (N**2):
+        if 0 <= i + di[d] < N and 0 <= j + dj[d] < N:
+            # 이미 채운 칸인 경우
+            if arr[i + di[d]][j + dj[d]] != 0:
+                d = ((d + 1) % 4 )
+                continue
+            else:
+                i += di[d]
+                j += dj[d]
+                num += 1
+                arr[i][j] = num
+        # 범위 벗어 나는 경우
+        else:
+            d = ((d + 1) % 4 )
+            continue
 
 for tc in range(1, T+1):
     N = int(input())
-    num_list = [[0] * N for _ in range(N)]
-    value = 1
-    ni = nj = 0
-    for n in range(N, 0, -2):
-        for d in range(4):
-            while (0 <= ni < N) and (0 <= nj < N):
-                num_list[ni][nj] = value
-                if not ((0 <= ni+di[d] < N) and (0 <= nj+dj[d] < N)):
-                    break
-                if num_list[ni + di[d]][nj + dj[d]] != 0:
-                    break
-                value += 1
-                ni += di[d]
-                nj += dj[d]
+    arr = [[0] * N for _ in range(N)]
 
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    
+    snail(N)
+ 
     print('#{}'.format(tc))
-    for li in num_list:
-        print(' '.join(list(map(str,li))))
+    for ar in arr:
+        print(*ar)
