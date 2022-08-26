1bec082bd6ab0b69509810121e18424c3094a4b7
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-08-26 16:38:39 +0900

    [D2] Title: 파리퇴치3, Time: 217 ms, Memory: 62,248 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
@@ -1,26 +1,28 @@
 T = int(input())
 
-for tc in range(1, T+1):
-    N, M = list(map(int, input().split()))
-    fly = []
-    for _ in range(N):
-        fly.append(list(map(int, input().split()))) 
-    
-    di = [-1, 1, 0, 0, -1, -1, 1, 1]   # + 모양 / x 모양
-    dj = [0, 0, -1, 1, -1, 1, -1, 1]   # 0~3, 4~7
+di = [-1, 0, 1, 0, -1, -1, 1, 1]
+dj = [0, 1, 0, -1, -1, 1, 1, -1]
 
-    max_fly = 0
-    for i in range(N):    # 노즐의 중심 
-        for j in range(N):  
-            for delta in [0, 4]: # 델타 0~3, 4~7
-                fly_cnt = fly[i][j]
-                for d in range(delta, 4 + delta): # 방향
-                    for k in range(1,M):
-                        if ((0 <= (i + (di[d] * k)) < N) and (0 <= (j + (dj[d] * k)) < N)):
-                            fly_cnt += fly[i + (di[d] * k)][j + (dj[d] * k)]
+for tc in range(1, T + 1):
+    N, M = list(map(int, input().split()))
+    map_list = list()
+
+    for _ in range(N):
+        map_list.append(list(map(int, input().split())))
+    
+    answer = 0
+
+    for i in range(N):
+        for j in range(N):
+            for d in [0, 4]:
+                tmp = map_list[i][j]
+                for dij in range(4):
+                    for m in range(1, M):
+                        if (0 <= i + (m * di[d + dij]) < N) and (0 <= j + (m * dj[d + dij]) < N):
+                            tmp += map_list[i + (m * di[d + dij])][j + (m * dj[d + dij])]
                         else:
                             break
-                if fly_cnt > max_fly:
-                    max_fly = fly_cnt
-
-    print('#{} {}'.format(tc, max_fly))
+                if tmp > answer:
+                    answer = tmp
+    
+    print('#{} {}'.format(tc, answer))
