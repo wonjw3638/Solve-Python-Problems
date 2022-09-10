8f2f3adde3894aefda0ea5dfb2e7e629a2cfad4b
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-11 00:38:54 +0900

    [Bronze I] Title: 색종이, Time: 736 ms, Memory: 123124 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -5,28 +5,23 @@ N = int(input())
 
 map_list = [[0] * 1001 for _ in range(1001)]
 
-if N == 1:
-    x, y, w, h = list(map(int, input().split()))
-    print(w * h)
+paper = list()
+answer = list()
 
-else:
-    paper = list()
-    answer = list()
+for _ in range(N):
+    paper.append(list(map(int, input().split())))
 
-    for _ in range(N):
-        paper.append(list(map(int, input().split())))
-    
-    for idx in range(N-1, -1, -1):
-        x, y, w, h = paper[idx]
-        tmp = 0
-        for j in range(w):
-            for i in range(h):
-                if map_list[y+i][x+j] > 0:
-                    continue
-                else:
-                    map_list[y+i][x+j] = 1
-                    tmp += 1
-        answer.append(tmp)
-    
-    for idx in range(N-1, -1, -1):
-        print(answer[idx])
+for idx in range(N-1, -1, -1):
+    x, y, w, h = paper[idx]
+    tmp = 0
+    for j in range(w):
+        for i in range(h):
+            if map_list[y+i][x+j] > 0:
+                continue
+            else:
+                map_list[y+i][x+j] = 1
+                tmp += 1
+    answer.append(tmp)
+
+for idx in range(N-1, -1, -1):
+    print(answer[idx])
