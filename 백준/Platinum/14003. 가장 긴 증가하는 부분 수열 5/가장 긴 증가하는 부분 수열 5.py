6c04a8d27d9b1ff3e925f78d6cc8a389bced864c
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-13 06:43:12 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 1700 ms, Memory: 157880 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left
+
+N = int(input())
+numbers = list(map(int, input().split()))
+Lis = list()
+idx_list = list()
+
+for i in numbers:
+    k = bisect_left(Lis, i)
+    idx_list.append(k)
+
+    if len(Lis) <= k: 
+        Lis.append(i)
+    else:
+        Lis[k] = i 
+        
+len_Lis = len(Lis)
+idx_list.reverse()
+numbers.reverse()
+
+answer = list()
+len_Lis -= 1
+
+for idx in range(N):
+    if idx_list[idx] == len_Lis:
+        answer.append(numbers[idx])
+        len_Lis -= 1
+
+answer.reverse()
+
+print(len(answer))
+print(*answer)
