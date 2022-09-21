c310828fa80487b911f75ad20f81fee28884f659
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-21 22:04:46 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,28 @@
+# 15664 N과 M 10
+# 220921
+
+# 중복 조합 사용
+def comb(arr, n):
+    result = list()
+
+    if n == 0:
+        return [[]]
+
+    for i in range(len(arr)):
+        elem = arr[i]
+        for j in comb(arr[i+1:], n-1):
+            result.append([elem] + j)
+    
+    return result
+
+N, M = list(map(int, input().split()))
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+arrComb = comb(arr, M)
+arrComb = list(set(map(tuple, arrComb)))
+arrComb.sort()
+
+for answer in arrComb:
+    print(*answer)
