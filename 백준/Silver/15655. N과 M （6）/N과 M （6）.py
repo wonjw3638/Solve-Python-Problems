1723d8390009ed422d2fb856e6f3e0d78d19ccc2
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-21 20:26:23 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,26 @@
+# 15655 N과 M 6
+# 220921
+
+# 조합 사용
+def comb(arr, n):
+    result = list()
+
+    if n == 0:
+        return [[]]
+
+    for i in range(len(arr)):
+        elem = arr[i]
+        for j in comb(arr[i+1:], n-1):
+            result.append([elem] + j)
+    
+    return result
+
+N, M = list(map(int, input().split()))
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+arrComb = comb(arr, M)
+
+for answer in arrComb:
+    print(*answer)
