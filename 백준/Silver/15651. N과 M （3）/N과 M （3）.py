869e097012e6e66146e978c849997829ab4e7123
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-21 20:18:37 +0900

    [Silver III] Title: N과 M (3), Time: 4824 ms, Memory: 155404 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,26 @@
+# 15651 N과 M 3
+# 220921
+
+# 중복순열 사용
+def perm(arr, n):
+    result = list()
+
+    if n == 0:
+        return [[]]
+
+    for i in range(len(arr)):
+        elem = arr[i]
+        for j in perm(arr, n-1):
+            result.append([elem] + j)
+    
+    return result
+
+N, M = list(map(int, input().split()))
+
+# 1부터 N까지 배열
+arr = [ i for i in range(1, N + 1)]
+
+arrPerm = perm(arr, M)
+
+for answer in arrPerm:
+    print(*answer)
