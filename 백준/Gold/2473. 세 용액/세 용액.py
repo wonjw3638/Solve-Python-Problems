bc239bea3eb9b2933b4510dd213eb030b3dbf52e
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-09-24 13:13:35 +0900

    [Gold III] Title: 세 용액, Time: 160 ms, Memory: 115400 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -0,0 +1,33 @@
+# 2473 세용액
+# 220924
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+answer = 3000000000
+answer_arr = list()
+
+for idx in range(N-2):
+    startIdx = idx+1
+    endIdx = N-1
+
+
+    while startIdx < endIdx:
+        total = arr[idx] + arr[startIdx] + arr[endIdx]
+        
+        if abs(total) < answer:
+            answer = abs(total)
+            answer_arr = [arr[idx], arr[startIdx], arr[endIdx]]
+        
+        if total > 0:
+            endIdx -= 1
+        elif total < 0:
+            startIdx += 1
+        else:
+            break
+    
+    if answer == 0:
+        break
+
+print(*answer_arr)
