939480ff41a4441be6e02db7dab07ea39d222932
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-10-02 22:08:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 464 ms, Memory: 42020 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,16 @@
+mainString = input()
+findString = list(input())
+
+N = len(findString)       
+stack = list()
+
+for char in mainString:
+    stack.append(char)
+    if char == findString[-1] and findString == stack[-N:]:
+        for _ in range(N):
+            stack.pop()
+
+if stack:
+    print(''.join(stack))
+else:
+    print("FRULA")
