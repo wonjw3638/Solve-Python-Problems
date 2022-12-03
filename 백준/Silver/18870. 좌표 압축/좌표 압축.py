18c47bc39d6d63f218e74efeb5f8075f48361fc6
지원 <109324634+wonjw3638@users.noreply.github.com>
2022-12-04 02:33:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1036 ms, Memory: 306212 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,20 @@
+# 전체 좌표개수 N
+N = int(input())
+# 좌표 리스트
+Xarr = list(map(int, input().split()))
+# 정렬된 좌표, 중복제거
+Xsort = list(sorted(set(Xarr)))
+# 정답을 저장할 list
+answer = list()
+# 압축된 좌표값을 저장할 dictionary
+Xdic = dict()
+
+# 반복문 돌면서 압축된 좌표값을 dictionary에 저장
+for idx, x in enumerate(Xsort):
+    Xdic[x] = idx
+
+# 반복문을 돌면서 정렬된 index값을 추가
+for x in Xarr:
+    answer.append(Xdic.get(x))
+
+print(*answer)
